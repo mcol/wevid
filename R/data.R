7d47b238d73cd191617e6466e41315b129c5c99c
Marco Colombo <m.colombo@ed.ac.uk>
2018-06-07 16:43:51 +0000

    Document all datasets in the same page.

--- R/data.R
+++ R/data.R
@@ -1,54 +1,40 @@
-#' Example dataset based on cross-validated prediction of coronary disease
-#' in the Cleveland Heart Study 
+#' Example datasets
 #'
-#' @format A data frame with 768 rows and three variables
-#' \describe{
-#'   \item{prior.p}{prior probabilities of case status}
-#'   \item{posterior.p}{posterior probabilities of case status}
-#'   \item{y}{case-control status}
+#' \code{cleveland} is based on cross-validated prediction of coronary disease
+#' in the Cleveland Heart Study (297 observations).
+#'
+#' @name datasets
+#' @usage NULL
+#' @format Each dataset consists of a data frame with the following variables:
+#' \itemize{
+#'   \item prior.p: Prior probabilities of case status.
+#'   \item posterior.p: Posterior probabilities of case status.
+#'   \item y: Case-control status.
 #' }
 #' @source \url{http://www.homepages.ed.ac.uk/pmckeigu/preprints/classify/demoplotw.html}
 "cleveland"
+
+#' \code{pima} is based on cross-validated prediction of diabetes
+#' in Pima Native Americans (768 observations).
 #'
-#' Example dataset based on cross-validated prediction of diabetes
-#' in Pima Native Americans
-#'
-#' @format A data frame with 297 rows and three variables
-#' \describe{
-#'   \item{prior.p}{prior probabilities of case status}
-#'   \item{posterior.p}{posterior probabilities of case status}
-#'   \item{y}{case-control status}
-#' }
-#' @source \url{http://www.homepages.ed.ac.uk/pmckeigu/preprints/classify/demoplotw.html}
+#' @usage NULL
+#' @rdname datasets
 "pima"
+
+#' \code{fitonly} is based on cross-validated prediction of colorectal cancer
+#' from fecal immunochemical test (FIT) only in Michigan. As most controls and
+#' some cases have have zero values in the FIT test, to fit densities to the
+#' sampled values of weight of evidence in controls and cases it is necessary
+#' to specify spike-slab mixtures (242 observations).
 #'
-#' Example dataset based on cross-validated prediction of colorectal cancer 
-#' from fecal immunochemical test (FIT) only in Michigan.  As most controls and
-#' some cases have have zero values in the FIT test, to fit densities to the sampled
-#' values of weight of evidence in controls and cases it is necessary to specify spike-slab
-#' mixtures. 
-#'
-#' @format A data frame with 242 rows and three variables
-#' \describe{
-#'   \item{prior.p}{prior probabilities of case status}
-#'   \item{posterior.p}{posterior probabilities of case status}
-#'   \item{y}{case-control status}
-#' }
-#' @source \url{http://www.homepages.ed.ac.uk/pmckeigu/preprints/classify/demoplotw.html}
+#' @usage NULL
+#' @rdname datasets
 "fitonly"
+
+#' \code{fitplusmicrobiome} is based on cross-validated prediction of colorectal
+#' cancer from fecal immunochemical test (FIT) plus microbiome profile in
+#' Michigan (242 observations).
 #'
-#' Example dataset based on cross-validated prediction of colorectal cancer
-#' from fecal immunochemical test (FIT) plus microbiome profile in Michigan
-#'
-#' @format A data frame with 242 rows and three variables
-#' \describe{
-#'   \item{prior.p}{prior probabilities of case status}
-#'   \item{posterior.p}{posterior probabilities of case status}
-#'   \item{y}{case-control status}
-#' }
-#' @source \url{http://www.homepages.ed.ac.uk/pmckeigu/preprints/classify/demoplotw.html}
+#' @usage NULL
+#' @rdname datasets
 "fitplusmicrobiome"
-#'
- 
-
-
