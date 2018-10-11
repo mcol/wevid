ab276e5e5fe5890c1138967cf14a1b0659d8b9a8
Marco Colombo <m.colombo@ed.ac.uk>
2018-10-11 09:33:50 +0000

    Update tests.

--- tests/dump_wevid.R
+++ tests/dump_wevid.R
@@ -1,24 +1,24 @@
 s1.ok <-
-structure(list(`Cases / controls` = structure(1L, .Label = "137 / 160", class = "factor"), 
-    `Crude C-statistic` = 0.895, `Model-based C-statistic` = 0.915, 
-    `Crude Lambda (bits)` = 2.49, `Model-based Lambda (bits)` = 2.65, 
-    `Test log-likelihood (natural log units)` = -119.18), .Names = c("Cases / controls", 
-"Crude C-statistic", "Model-based C-statistic", "Crude Lambda (bits)", 
-"Model-based Lambda (bits)", "Test log-likelihood (natural log units)"
+structure(list(`Cases / controls` = structure(1L, .Label = "137 / 160", class = "factor"),
+    `Crude C-statistic` = 0.895, `Model-based C-statistic` = 0.9,
+    `Crude Λ (bits)` = 2.49, `Model-based Λ (bits)` = 2.31,
+    `Test log-likelihood (nats)` = -119.18, `log-likelihood after recalibration (nats)` = -118.69), .Names = c("Cases / controls",
+"Crude C-statistic", "Model-based C-statistic", "Crude Λ (bits)",
+"Model-based Λ (bits)", "Test log-likelihood (nats)", "log-likelihood after recalibration (nats)"
 ), row.names = c(NA, -1L), class = "data.frame")
 s2.ok <-
-structure(list(`Cases / controls` = structure(1L, .Label = "268 / 500", class = "factor"), 
-    `Crude C-statistic` = 0.837, `Model-based C-statistic` = 0.839, 
-    `Crude Lambda (bits)` = 1.33, `Model-based Lambda (bits)` = 1.37, 
-    `Test log-likelihood (natural log units)` = -364.32), .Names = c("Cases / controls", 
-"Crude C-statistic", "Model-based C-statistic", "Crude Lambda (bits)", 
-"Model-based Lambda (bits)", "Test log-likelihood (natural log units)"
+structure(list(`Cases / controls` = structure(1L, .Label = "268 / 500", class = "factor"),
+    `Crude C-statistic` = 0.837, `Model-based C-statistic` = 0.834,
+    `Crude Λ (bits)` = 1.33, `Model-based Λ (bits)` = 1.31,
+    `Test log-likelihood (nats)` = -364.32, `log-likelihood after recalibration (nats)` = -364.25), .Names = c("Cases / controls",
+"Crude C-statistic", "Model-based C-statistic", "Crude Λ (bits)",
+"Model-based Λ (bits)", "Test log-likelihood (nats)", "log-likelihood after recalibration (nats)"
 ), row.names = c(NA, -1L), class = "data.frame")
 s3.ok <-
-structure(list(`Cases / controls` = structure(1L, .Label = "101 / 141", class = "factor"), 
-    `Crude C-statistic` = 0.893, `Model-based C-statistic` = 0.93, 
-    `Crude Lambda (bits)` = 3.08, `Model-based Lambda (bits)` = 3.03, 
-    `Test log-likelihood (natural log units)` = -73.26), .Names = c("Cases / controls", 
-"Crude C-statistic", "Model-based C-statistic", "Crude Lambda (bits)", 
-"Model-based Lambda (bits)", "Test log-likelihood (natural log units)"
+structure(list(`Cases / controls` = structure(1L, .Label = "101 / 141", class = "factor"),
+    `Crude C-statistic` = 0.893, `Model-based C-statistic` = 0.924,
+    `Crude Λ (bits)` = 3.08, `Model-based Λ (bits)` = 2.88,
+    `Test log-likelihood (nats)` = -73.26, `log-likelihood after recalibration (nats)` = -73.19), .Names = c("Cases / controls",
+"Crude C-statistic", "Model-based C-statistic", "Crude Λ (bits)",
+"Model-based Λ (bits)", "Test log-likelihood (nats)", "log-likelihood after recalibration (nats)"
 ), row.names = c(NA, -1L), class = "data.frame")
